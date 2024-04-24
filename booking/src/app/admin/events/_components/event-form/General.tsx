d4007362d303b779b00d929e50519117b8ab4a3e
kaiotcp1 <kaiotcp1@gmail.com>
2024-04-24 19:46:49 -0300

    BUGFIX - Component step fix

--- booking/src/app/admin/events/_components/event-form/General.tsx
+++ booking/src/app/admin/events/_components/event-form/General.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Input, Textarea } from '@nextui-org/react';
+import React, { useEffect } from 'react'
+import { Button, Input, Textarea, Badge } from '@nextui-org/react';
+import { Chip } from "@nextui-org/react";
+import { trusted } from 'mongoose';
 
- 
 export interface EventFormStepProps {
   event: any,
   setEvent: React.Dispatch<React.SetStateAction<any>>,
@@ -10,26 +11,101 @@ export interface EventFormStepProps {
 };
 
 const General = ({ event, activeStep, setActiveStep, setEvent }: EventFormStepProps) => {
- 
+
+  const [guest, setGuest] = React.useState<string>('');
+
   const getCommonProps = (name: string) => {
     return {
-      labelPlacement: 'outside',
+      labelPlacement: 'outside' as 'outside',
       value: event?.[name],
-      onchange: (e: any) => setEvent({...event, [name]: e.target.value}),
-    } as any;
+      onChange: (e: any) => {
+        setEvent({ ...event, [name]: e.target.value });
+        isFormValid();
+      },
+      isRequired: isFormValid(),
+    };
   };
-  
+
+  const teste = useEffect(() => {
+    console.log(event);
+    console.log(isFormValid());
+
+  }, [event]);
+
+
+
+  const onGuestAdd = () => {
+    const newGuests = [];
+    const commaSeparatedGuests = guest.split(',')
+
+    if (commaSeparatedGuests.length > 1) {
+      newGuests.push(...commaSeparatedGuests);
+    } else {
+      newGuests.push(guest);
+    };
+
+    if (event?.guests) {
+      newGuests.push(...event.guests);
+    }
+
+    setEvent({ ...event, guests: newGuests });
+    setGuest('');
+  };
+
+  const onGuestRemove = (guestToRemove: number) => {
+    const newGuests = event?.guests?.filter((_: string, index: number) => index !== guestToRemove);
+    setEvent({ ...event, guests: newGuests });
+  };
+
+  const isFormValid = () => {
+    if (!event?.name || event.name.length < 4 ||
+      !event?.organizer || event.organizer.length < 2 ||
+      !event?.description || event.description.length < 10) {
+      return true
+    } else {
+      return false;
+    }
+  };
+
+
+
   return (
     <div className='flex flex-col gap-5'>
       <Input label='Event name' placeholder='Enter event name'
         {...getCommonProps('name')} />
 
-        <Input label='Organizer'
+      <Input label='Organizer'
         placeholder='Enter organizer name'
-        {...getCommonProps('Organizer')}/>
+        {...getCommonProps('organizer')} />
 
-        <Textarea placeholder='Enter description'
-        label='Description' {...getCommonProps('Description')} />
+      <Textarea placeholder='Enter description'
+        label='Description' {...getCommonProps('description')} />
+
+      <div className="flex items-end gap-5">
+        <Input
+          placeholder='Enter your guests'
+          label='Guests'
+          value={guest}
+          onChange={(e) => setGuest(e.target.value)}
+          labelPlacement='outside'
+        />
+        <Button
+          onClick={onGuestAdd}
+          className='bg-gray-700 text-white'
+          title='Add'>Add</Button>
+      </div>
+      <div className="flex flex-wrap gap-5">
+        {event?.guests?.map((guest: string, index: number) => (
+          <Chip
+            onClose={() => onGuestRemove(index)}
+            className='text-black'>{guest}</Chip>
+        ))}
+      </div>
+      <div className="flex justify-center gap-5">
+        <Button className='bg-gray-200 text-black' onClick={() => { }}>Cancel</Button>
+        <Button className='bg-gray-700 text-white' onClick={() => setActiveStep(activeStep + 1)}
+          isDisabled={isFormValid()}>Next</Button>
+      </div>
     </div>
   )
 }
